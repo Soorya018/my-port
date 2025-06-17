48cd18852462ce1565ec4df73211699b3979ce12
Arun Soorya Kumaresan <arunsoorya@Aruns-MacBook-Air.local>
2025-06-17 14:53:24 +0100

    Updates

---
+++ precache-manifest.b3fcfc6392174a775fa471d4d0658eb6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9586e9e00ff5d1d9690da6a9a04f291",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6b3b1fa802a5939b2f9",
+    "url": "/static/css/main.728d2373.chunk.css"
+  },
+  {
+    "revision": "9eae98fa22a0e4dc39a2",
+    "url": "/static/js/2.1015303d.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/static/js/2.1015303d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6b3b1fa802a5939b2f9",
+    "url": "/static/js/main.8db234d5.chunk.js"
+  },
+  {
+    "revision": "e8d5e68c52ed10ca20b4",
+    "url": "/static/js/runtime-main.2d4e5be7.js"
+  },
+  {
+    "revision": "a9f7d88c90848313ce16d8205c8c02ff",
+    "url": "/static/media/picture.a9f7d88c.png"
+  },
+  {
+    "revision": "e1688000ac00c34677c5fc1a6350c58c",
+    "url": "/static/media/resume.e1688000.pdf"
+  },
+  {
+    "revision": "e65d5741c285403eab3ce6f8ec9da5ea",
+    "url": "/static/media/zoho.e65d5741.png"
+  }
+]);
