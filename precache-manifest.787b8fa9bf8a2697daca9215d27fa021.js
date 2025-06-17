efa8dace369e8e5f5e243372eb29b2138f00c217
Arun Soorya Kumaresan <arunsoorya@Aruns-MacBook-Air.local>
2025-06-17 13:04:14 +0100

    Updates

---
+++ precache-manifest.787b8fa9bf8a2697daca9215d27fa021.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae68eba7ed94f5acf050a79376416b41",
+    "url": "/my-port/index.html"
+  },
+  {
+    "revision": "07ab4875fd5d01067e4f",
+    "url": "/my-port/static/css/main.728d2373.chunk.css"
+  },
+  {
+    "revision": "9eae98fa22a0e4dc39a2",
+    "url": "/my-port/static/js/2.1015303d.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/my-port/static/js/2.1015303d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07ab4875fd5d01067e4f",
+    "url": "/my-port/static/js/main.2b49a831.chunk.js"
+  },
+  {
+    "revision": "10be1d33b467535e1179",
+    "url": "/my-port/static/js/runtime-main.81961cf4.js"
+  },
+  {
+    "revision": "a9f7d88c90848313ce16d8205c8c02ff",
+    "url": "/my-port/static/media/picture.a9f7d88c.png"
+  },
+  {
+    "revision": "e1688000ac00c34677c5fc1a6350c58c",
+    "url": "/my-port/static/media/resume.e1688000.pdf"
+  },
+  {
+    "revision": "e65d5741c285403eab3ce6f8ec9da5ea",
+    "url": "/my-port/static/media/zoho.e65d5741.png"
+  }
+]);
